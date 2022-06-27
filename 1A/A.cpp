63bd3b55f9d66d8ca90dd28a44039ae0fb7f3d14
Vasiliy <52681532+vaZZZy1@users.noreply.github.com>
2022-06-27 18:49:55 +0300

    Create A.cpp

---
+++ 1A/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
